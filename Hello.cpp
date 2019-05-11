0f8e2561cfa80ca091d7e6b9424f09aff7dd392e
root <root@custVBOX.(none)>
2019-05-11 21:26:35 +0800

    new file hello for test

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: Hello.cpp
+	> Author: lcc
+	> Mail: 983938706@qq.com 
+	> Created Time: Sat 11 May 2019 09:21:59 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	std::cout << "hello" ;
+	return 0;
+}
+
